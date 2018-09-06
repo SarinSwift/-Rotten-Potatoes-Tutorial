3114df604a7a784e6fbcaa26d2f46ea7d080f356
ruhsane <ruhsane924@gmail.com>
2018-09-06 10:21:09 -0700

    Users can see all reviews

--- app.js
+++ app.js
@@ -4,11 +4,22 @@ var exphbs = require('express-handlebars');
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
-
-app.get('/', (req, res) => {
-    res.render('home', { msg: 'Hello World!' });
-})
+// 
+// app.get('/', (req, res) => {
+//     res.render('home', { msg: 'Hello World!' });
+// })
 
 app.listen(3000, () => {
   console.log('App listening on port 3000!')
+})
+
+// OUR MOCK ARRAY OF PROJECTS
+let reviews = [
+  { title: "Great Review" },
+  { title: "Next Review" }
+]
+
+// INDEX
+app.get('/', (req, res) => {
+  res.render('reviews-index', { reviews: reviews });
 })
