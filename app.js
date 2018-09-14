aa664eb12773c63293d25efb6a0d1772e9997b67
ruhsane <ruhsane924@gmail.com>
2018-09-14 09:54:08 -0700

    Users can see single reviews

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ mongoose.connect('mongodb://localhost/rotten-potatoes', { useMongoClient: true }
 const Review = mongoose.model('Review', {
   title: String,
   description: String,
-  movieTitle: String
+  movieTitle: String,
+  rating: Number
 });
 
 // INITIALIZE BODY-PARSER AND ADD IT TO APP
@@ -22,9 +23,7 @@ var exphbs = require('express-handlebars');
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-app.listen(3000, () => {
-  console.log('App listening on port 3000!')
-})
+
 
 // INDEX
 app.get('/', (req, res) => {
@@ -45,9 +44,22 @@ app.get('/reviews/new', (req, res) => {
 // CREATE
 app.post('/reviews', (req, res) => {
   Review.create(req.body).then((review) => {
-    console.log(review);
-    res.redirect('/');
+    console.log(review)
+    res.redirect(`/reviews/${review._id}`) // Redirect to reviews/:id
+  }).catch((err) => {
+    console.log(err.message)
+  })
+})
+
+// SHOW
+app.get('/reviews/:id', (req, res) => {
+  Review.findById(req.params.id).then((review) => {
+    res.render('reviews-show', { review: review })
   }).catch((err) => {
     console.log(err.message);
   })
+})
+
+app.listen(3000, () => {
+  console.log('App listening on port 3000!')
 })
